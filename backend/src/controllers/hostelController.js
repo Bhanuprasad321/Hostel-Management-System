d31511f77a4fd5416a8e0c76b098c873a26e93e6
Bhanuprasad321 <kanugulabhanu012@gmail.com>
2026-06-01 14:47:40 +0530

    Implemented hostel admin management APIs

--- backend/src/controllers/hostelController.js
+++ backend/src/controllers/hostelController.js
@@ -1,35 +1,95 @@
-const {db} = require('../config/mysql');
-
-const createHostel = async (req,res) => {
-    try {
-        const {hostel_name,address} = req.body;
-        if (!hostel_name || !address) {
-            return res.status(400).json({message: "All fields are required"});
-        }
-        const [rows]= await db.promise().query('SELECT * FROM hostels WHERE hostel_name = ?',[hostel_name]);
-        if (rows.length!==0) {
-            return res.status(409).json({message: "Hostel with entered name already exists"});
-        }
-        await db.promise().query('INSERT INTO hostels (hostel_name,address) VALUES (?,?)',[hostel_name,address]);
-        return res.status(200).json({message: "New hostel is created"})
-
+const { db } = require("../config/mysql");
+const bcrypt = require("bcrypt");
+const createHostel = async (req, res) => {
+  try {
+    const { hostel_name, address } = req.body;
+    if (!hostel_name || !address) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    catch (err) {
-        console.log(err);
-        return res.status(500).json({message: "Internal server error"});
+    const [rows] = await db
+      .promise()
+      .query("SELECT * FROM hostels WHERE hostel_name = ?", [hostel_name]);
+    if (rows.length !== 0) {
+      return res
+        .status(409)
+        .json({ message: "Hostel with entered name already exists" });
     }
-}
+    await db
+      .promise()
+      .query("INSERT INTO hostels (hostel_name,address) VALUES (?,?)", [
+        hostel_name,
+        address,
+      ]);
+    return res.status(200).json({ message: "New hostel is created" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
+const getHostels = async (req, res) => {
+  try {
+    const [hostels] = await db.promise().query("SELECT * FROM hostels");
+    return res.status(200).json(hostels);
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-const getHostels = async (req,res) => {
-    try {
-        const [hostels] = await db.promise().query('SELECT * FROM hostels');
-        return res.status(200).json(hostels);
-    }   
-    catch (err) {
-        return res.status(500).json({message: "Internal Server Error"});
+const createHostelAdmin = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const  hostel_id  = parseInt(req.params.id);
+    const role = "admin";
+    if (!name || !email || !password || !hostel_id) {
+      return res.status(400).json({ message: "All fields are required!" });
     }
-}
+    const [hostel] = await db
+      .promise()
+      .query("SELECT * FROM hostels WHERE id = ?", [hostel_id]);
+    if (hostel.length === 0)
+      return res
+        .status(404)
+        .json({ message: "No hostel exist with hostel id" });
+    const [dupEmail] = await db
+      .promise()
+      .query("SELECT * FROM users WHERE email = ?", [email]);
+    if (dupEmail.length !== 0) {
+      return res
+        .status(400)
+        .json({ message: "Already user existed with entered Email" });
+    }
+    const hashedPass = await bcrypt.hash(password, 10);
 
+    await db
+      .promise()
+      .query(
+        "INSERT INTO users (name,email,password,hostel_id,role) VALUES (?,?,?,?,?)",
+        [name, email, hashedPass, hostel_id, role],
+      );
+    return res.status(200).json({ message: "New hostel admin is created" });
+  } catch {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-module.exports = {createHostel,getHostels}
+const getHostelAdmins = async (req, res) => {
+  try {
+    const hostel_id = parseInt(req.params.id);
+    const [hostelAdmins] = await db
+      .promise()
+      .query("SELECT id,name,email,role,hostel_id FROM users WHERE hostel_id = ? AND role = 'admin'", [
+        hostel_id,
+      ]);
+    return res.status(200).json(hostelAdmins);
+  } catch (err) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = {
+  createHostel,
+  getHostels,
+  createHostelAdmin,
+  getHostelAdmins,
+};
--- backend/src/routes/hostelRoutes.js
+++ backend/src/routes/hostelRoutes.js
@@ -1,9 +1,11 @@
 const { superAdminOnly, protect } = require("../middleware/authMiddleware");
 const express = require("express");
 const route = express.Router();
-const { createHostel, getHostels } = require("../controllers/hostelController");
+const { createHostel, getHostels,createHostelAdmin,getHostelAdmins } = require("../controllers/hostelController");
 
 route.post("/", protect, superAdminOnly, createHostel);
 route.get("/", protect, superAdminOnly, getHostels);
+route.post("/:id/admin/",protect,superAdminOnly,createHostelAdmin);
+route.get("/:id/admin/",protect,superAdminOnly,getHostelAdmins);
 
 module.exports = route;
